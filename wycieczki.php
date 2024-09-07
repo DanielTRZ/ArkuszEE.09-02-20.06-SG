2ed138f93c6778bdc1c44cfdcecaf0c74aa30873
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-09-07 19:55:56 +0200

    Update wycieczki.php

--- wycieczki.php
+++ wycieczki.php
@@ -35,7 +35,7 @@ echo "<img src=\"zdjecia/$nazwa\" alt='galeria'> ";
 <td>Jesień</td><td>Grupa 4+</td> <td>Grupa 10+</td>   
 </tr>
 <tr>
-           <td>5%</td><td>10%</td><td>15%</td>
+        <td>5%</td><td>10%</td><td>15%</td>
         </tr>
         </table>
     </div> 
